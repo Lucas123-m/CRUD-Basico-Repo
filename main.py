2503159230f80643f6914c79c2077dbc9e6ddd96
Lucas <lucasmio98@gmail.com>
2022-12-29 20:58:25 -0300

    Actualizo funciones en design_window

--- main.py
+++ main.py
@@ -5,29 +5,27 @@ import sqlite3
 DB_PATH = "main_db.db"
 
 class Crud:
+	labels_entries = ["ID","Name","Surname","Age","Address"]
+
 	def __init__(self):
-		self.ventana = tk.Tk()
-		self.diseñar_ventana(self.ventana)
-		self.ventana.mainloop()
+		self.window = tk.Tk()
+		self.design_window(self.window)
+		self.window.mainloop()
 
-	def diseñar_ventana(self,ventana):
+	def design_window(self,window):
+		self.design_menu()
 
-		menu = tk.Menu(self.ventana)
-		ventana.config(menu=menu)
-		menu_inicio = tk.Menu(menu,tearoff=0)
-		menu_inicio.add_command(label="Conectar con base de datos",command=self.conectar_bbdd)
+		window.title("Prueba")
+		self.design_labels()
 
-		menu.add_cascade(label="Inicio",menu=menu_inicio)
-
-		ventana.title("Prueba")
-		labels_entries = ["ID","Name","Surname","Age","Address",]
-
-		frame = tk.LabelFrame(ventana,text="Fields of data",width=10)
+	def design_labels(self):
+		frame = tk.LabelFrame(window,text="Fields of data",width=10)
 		frame.grid(row=0,column=1,pady=5,padx=10)
 
 		for i,label in enumerate(labels_entries):
 			tk.Label(frame,text=label + " :").grid(row=i,column=0,padx=5,pady=3)
 
+	def design_entries(self):
 		entries = ["ID","name","surname","age","addres"]
 		dict_entries = {entry:"" for entry in entries}
 
@@ -35,33 +33,44 @@ class Crud:
 			dict_entries[entry] = tk.Entry(frame)
 			dict_entries[entry].grid(row=i,column=1,padx=5,pady=3)
 
-		ttk.Button(ventana,text="NUEVO",width=20).grid(row=i+1,column=0,pady=5)
-		ttk.Button(ventana,text="GUARDAR",width=20).grid(row=i+1,column=1,pady=5)
-		ttk.Button(ventana,text="CANCELAR",width=20).grid(row=i+1,column=2,pady=5)
+	def design_menu(self):
+		menu = tk.Menu(self.window)
+		window.config(menu=menu)
+		menu_inicio = tk.Menu(menu,tearoff=0)
+		menu_inicio.add_command(label="Connect with Database",command=self.conectar_bbdd)
 
-		self.tree = ttk.Treeview(ventana,height=10,column=(0,1,2,3))
+		menu.add_cascade(label="File",menu=menu_inicio)
+
+	def design_treeview_and_buttons(self):
+
+		ttk.Button(window,text="NEW",width=20).grid(row=i+1,column=0,pady=5)
+		ttk.Button(window,text="SAVE",width=20).grid(row=i+1,column=1,pady=5)
+		ttk.Button(window,text="CANCEL",width=20).grid(row=i+1,column=2,pady=5)
+
+		self.tree = ttk.Treeview(window,height=10,column=(0,1,2,3))
 
 		self.tree.grid(row=i+2,column=0,columnspan=4,padx=5,pady=5,sticky=tk.W + tk.E),
 		
 		for i,column_name in enumerate(labels_entries):
+			print(i)
 			self.tree.heading("#" + str(i),text=column_name,anchor=tk.CENTER)
 			self.tree.column("#" + str(i),anchor=tk.CENTER)
 
-		ttk.Button(ventana,text="DELETE",width=20).grid(row=8,column=1,pady=5,padx=100)
-		ttk.Button(ventana,text="EDIT",width=20).grid(row=8,column=2,pady=5)
+		ttk.Button(window,text="DELETE",width=20).grid(row=8,column=1,pady=5,padx=100)
+		ttk.Button(window,text="EDIT",width=20).grid(row=8,column=2,pady=5)
 
 	def conectar_bbdd(self):
 		try:
 			query = "SELECT * FROM DATA"
 			data = self.run_query(query)
-			messagebox.showinfo("Conexion","Se ha conectado con la base de datos correctamente.")
+			messagebox.showinfo("Connection","The connection was correctly performed.")
 			for row in data:
 				self.tree.insert("",0,text=row[0],values=[row[1],row[2],row[3],row[4]])
 
 		except Exception as e:
-			msg = "Se ha producido el siguiente error al conectar con la tabla de la base de datos." + f"\n{e}"
+			msg = "An error occurs when connecting the database: " + f"\n{e}"
 			messagebox.showerror("Error",msg)
-			respuesta = messagebox.askyesno(title="Conexion",message="Desea crear una nueva tabla?")
+			respuesta = messagebox.askyesno(title="Connection",message="Do you want to create a new table?")
 			if respuesta:
 				try:
 					query = "CREATE TABLE DATA "  
@@ -80,8 +89,5 @@ class Crud:
 			result = cursor.fetchall()
 			cursor.close()
 		return result
-
-	def hola(self):
-		print("hola")
 
 crud = Crud()
